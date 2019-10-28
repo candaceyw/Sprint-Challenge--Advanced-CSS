6c456fb449db304aa5d62bcaa335b8f5074f1fc1
Candace Wilson <candacewilson@Candaces-MacBook-Air.local>
2019-10-27 20:36:01 -0400

    mobile version complete

---
+++ animation.js
@@ -0,0 +1,7 @@
+document.addEventListener('aos:in', ({ detail }) => {
+    console.log('animated in', detail);
+  });
+  
+  document.addEventListener('aos:out', ({ detail }) => {
+    console.log('animated out', detail);
+  });
